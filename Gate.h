3388966f07233d07ddd86e84041ddd956ebbde13
InhyeokJ <adcjih@kookmin.ac.kr>
2024-05-30 13:17:48 +0900

    Update Gate.h

--- Gate.h
+++ Gate.h
@@ -1,16 +1,15 @@
 #pragma once
 #include "DrawInter.h"
+#include <utility>
 #include "Map.h"
 
 class Gate : public DrawInter {
 public:
-    Gate(int BoardxSize, int BoardySize);
+    Gate();
     void draw() override;
     void generate();
-    int getX1() const;
-    int getY1() const;
-    int getX2() const;
-    int getY2() const;
+    pair<int, int> gate1;
+    pair<int, int> gate2;
 
 private:
     int gate1X, gate1Y, gate2X, gate2Y;
